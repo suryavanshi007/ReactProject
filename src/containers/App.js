5235dff6cfbfe57eb4f3ec7fd4d770f78ea6c04a
KRISHNA SURYAVANSHI <krishnasuryavanshi@MTXs-MacBook-Pro.local>
2022-03-01 17:41:13 +0530

    Added some UI changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,14 +7,12 @@ import './App.css';
 function App() {
   const [robots, setRobots] = useState([])
   const [searchfield, setSearchfield] = useState('')
-  const [count, setCount] = useState(0) // for demo purposes
 
   useEffect(()=> {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response=> response.json())
       .then(users => {setRobots(users)});
-    // console.log(count)
-  },[]) // if you add count, only run if count changes.
+  },[])
 
   const onSearchChange = (event) => {
     setSearchfield(event.target.value)
@@ -29,7 +27,6 @@ function App() {
     (
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
-        <button onClick={()=>setCount(count+1)}>Click Me!</button>
         <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           <CardList robots={filteredRobots} />
